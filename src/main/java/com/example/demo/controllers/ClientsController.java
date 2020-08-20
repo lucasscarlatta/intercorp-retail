533cf19127f4c6e45ad954ef2ccb85310843be9c
Lucas Scarlatta <lucasscarlatta@gmail.com>
2020-08-20 19:27:17 -0300

    Add swagger documentation

--- src/main/java/com/example/demo/controllers/ClientsController.java
+++ src/main/java/com/example/demo/controllers/ClientsController.java
@@ -2,11 +2,15 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Client;
 import com.example.demo.services.ClientService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -16,13 +20,17 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping(value = "/clients", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(tags = "Clients")
 public class ClientsController {
 
     @Autowired
     private ClientService service;
 
+    @ApiOperation(value = "Create client")
+    @ApiResponse(responseCode = "201", description = "New client created")
     @RequestMapping(method = POST, value = "")
+    @ResponseStatus(value = CREATED)
     public ResponseEntity<Client> create(@Valid @RequestBody Client client) {
-        return new ResponseEntity<Client>(service.create(client), CREATED);
+        return new ResponseEntity<>(service.create(client), CREATED);
     }
 }
--- src/main/java/com/example/demo/models/Client.java
+++ src/main/java/com/example/demo/models/Client.java
@@ -1,5 +1,6 @@
 package com.example.demo.models;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 import java.time.Period;
 
@@ -22,19 +25,24 @@ public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(hidden = true)
     private Long id;
 
     @NotNull
     @Column(name = "NAME")
+    @ApiModelProperty(required = true, value = "Client name", example = "Lucas")
     private String name;
 
     @Column(name = "LAST_NAME")
+    @ApiModelProperty(required = true, example = "Scarlatta")
     private String lastName;
 
     @Column(name = "BIRTHDAY")
+    @ApiModelProperty(required = true, example = "1992-10-16")
     private LocalDate birthday;
 
     @Transient
+    @ApiModelProperty(hidden = true)
     private Integer age;
 
     public Integer getAge() {
